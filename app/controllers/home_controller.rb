1782c9190e8d6ebdc623e6680e8783a8823ca9cc
ydarbleoj <brady.joel@gmail.com>
2016-03-04 12:56:59 -0800

    CSS Fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     if user_signed_in?
       redirect_to user_path(current_user.id)
      else
-     	redirect_to root_path
+     	root_path
     end
   end
 
