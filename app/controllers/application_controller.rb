2c1f9295ca06ea85a9c6b6eabf1ca94d9ca0ec1e
ydarbleoj <brady.joel@gmail.com>
2016-03-04 13:10:01 -0800

    CSS Fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
   def record_not_found
     redirect_to root_path
   end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u|
       u.permit(:password, :password_confirmation, :current_password)
