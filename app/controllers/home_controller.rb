4ed1a0368060973bcf790bb946c41b8a7d89e77f
ydarbleoj <brady.joel@gmail.com>
2016-03-05 11:10:33 -0800

    CSS Fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
 
 	def index
     if user_signed_in?
-      redirect_to user_path(current_user.id)
+      # redirect_to user_path(current_user.id)
+      redirect_to tournaments_path
     end
   end
 
