5b3bd12bc21ecad22df74fac71690190dd4ee87a
ydarbleoj <brady.joel@gmail.com>
2016-06-09 08:45:22 -0700

    Bugg fixes

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -48,7 +48,8 @@ class TournamentsController < ApplicationController
    p tourn = Tournament.where("end_date > ?", Date.today).first
    p "USer #{current_user.email}"
    p "Scorecard #{current_user.scorecards.first.id}"
-   p "Round #{current_user.tournament_rounds.where(tournament_id: tourn.id)}"
+   test = current_user.tournament_rounds.where(tournament_id: tourn.id).first
+   p "TEST #{test}"
    p  user_tourn = current_user.tournament_rounds.where(tournament_id: tourn.id).first
    p user_tourn = user_tourn.nil? ? nil : user_tourn.tournament
 
