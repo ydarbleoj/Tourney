01a61241b122236444d8f7175c50a39ed7cea70b
ydarbleoj <brady.joel@gmail.com>
2018-02-05 17:02:51 -0800

    updated team scorecard

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["sendgrid_name"],
+  :password => ENV["sendgrid_password"],
+  :domain => 'olition.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.raise_delivery_errors = true
