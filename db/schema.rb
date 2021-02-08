f7a1e2a87ae90a6a87a471fa129cef290daeaf59
ydarbleoj <brady.joel@gmail.com>
2021-02-08 13:40:36 -0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_165607) do
+ActiveRecord::Schema.define(version: 2021_02_08_213700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -335,6 +335,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_165607) do
     t.bigint "scorecard_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position", default: 0
     t.index ["scorecard_id"], name: "index_team_cards_on_scorecard_id"
     t.index ["team_id"], name: "index_team_cards_on_team_id"
   end
