cc6d2418740da588a62da61151b59b49ca4d3298
ydarbleoj <brady.joel@gmail.com>
2021-02-09 21:41:37 -0800

    wip

--- app/controllers/api/v2/rounds/lists_controller.rb
+++ app/controllers/api/v2/rounds/lists_controller.rb
@@ -27,7 +27,11 @@ module API
           return false if @tournament_user.blank?
 
           @tournament.tournament_rounds.map do |round|
-            Team.includes(:tournament_round, :scorecards).where(user_id: current_user.id, tournament_round_id: round.id).first
+            Team.includes(:tournament_round, :scorecards).where(
+              scorecards: {
+                user_id: current_user.id, tournament_round_id: round.id
+              }
+            ).first
           end
         end
 
