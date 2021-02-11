854289324daa3986d9b7d8307537ebcd347d92ed
ydarbleoj <brady.joel@gmail.com>
2021-02-11 06:52:20 -0800

    wip

--- app/serializers/tournament_create_serializer.rb
+++ app/serializers/tournament_create_serializer.rb
@@ -8,17 +8,16 @@ class TournamentCreateSerializer
   attributes :name, :year, :start_date, :end_date, :num_players, :num_rounds, :id
 
   attribute :round_info do |object|
-    return [] if object.tournament_rounds.blank?
-
     dates = (object.start_date.to_datetime..object.end_date.to_datetime).to_a
     (1..object.num_rounds).map do |i|
       round = object.tournament_rounds.find_by(:round_number => i)
+      course = round&.new_course
 
       {
         round_number: i,
         roundNumber: i,
-        course: round.new_course.name,
-        roundId: round.id,
+        course: new_course&.name,
+        roundId: round&.id,
         round_date: dates[i-1],
         display_date: dates[i-1].strftime("%a %d"),
       }
