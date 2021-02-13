d026f384cf792e35c895b261d9c30daa701e4f97
ydarbleoj <brady.joel@gmail.com>
2021-02-13 13:05:09 -0800

    Add gross data to TeamScorecard

--- app/serializers/round_info/team_card_serializer.rb
+++ app/serializers/round_info/team_card_serializer.rb
@@ -22,29 +22,70 @@ module RoundInfo
     end
 
     attribute :out_gross do |obj|
-      nil
+      objects = obj.team_scores.select { |x| x if x.number < 10 }
+      objects.map do |xx|
+        one = xx.score_1
+        two = xx.score_2
+        one_score = one ? one.score : 0
+        two_score = two ? two.score : 0
+
+        one_score + two_score
+      end.inject(0) { |sum, i| sum + i }
     end
 
     attribute :in_gross do |obj|
-      nil
+      objects = obj.team_scores.select { |x| x if x.number > 9 }
+      objects.map do |xx|
+        one = xx.score_1
+        two = xx.score_2
+        one_score = one ? one.score : 0
+        two_score = two ? two.score : 0
+
+        one_score + two_score
+      end.inject(0) { |sum, i| sum + i }
     end
 
     attribute :out_putts do |obj|
-      # obj.user_scores.select { |x| x if x.number < 10 }.map(&:putts).inject(0) { |sum, i| sum + i }
-      nil
+      objects = obj.team_scores.select { |x| x if x.number < 10 }
+      objects.map do |xx|
+        one = xx.score_1
+        two = xx.score_2
+        one_putts = one ? one.putts : 0
+        two_putts = two ? two.putts : 0
+
+        one_putts + two_putts
+      end.inject(0) { |sum, i| sum + i }
     end
 
     attribute :in_putts do |obj|
-      nil
-      # obj.user_scores.select { |x| x if x.number > 9 }.map(&:putts).inject(0) { |sum, i| sum + i }
+      objects = obj.team_scores.select { |x| x if x.number > 9 }
+      objects.map do |xx|
+        one = xx.score_1
+        two = xx.score_2
+        one_putts = one ? one.putts : 0
+        two_putts = two ? two.putts : 0
+
+        one_putts + two_putts
+      end.inject(0) { |sum, i| sum + i }
+    end
+
+    attribute :gross do |obj|
+      obj.team_scores.map do |team_score|
+        one = team_score.score_1
+        two = team_score.score_2
+        one_score = one ? one.score : 0
+        two_score = two ? two.score : 0
+
+        one_score + two_score
+      end.inject(0) { |sum, i| sum + i }
     end
 
     attribute :in_3putts do |obj|
       nil
     end
     attribute :out_3putts do |obj|
-      nil
       # obj.user_scores.select { |x| x if x.number < 10 && x.putts > 2 }.map(&:id).length
+      nil
     end
 
     attribute :course_name do |object|
@@ -55,19 +96,6 @@ module RoundInfo
       object.new_course.par
     end
 
-    attributes :players do |object|
-      object.scorecards.map do |scorecard|
-        {
-          first_name:   scorecard.user.first_name,
-          last_name:    scorecard.user.last_name,
-          scorecard_id: scorecard.id,
-          user_id:      scorecard.user.id,
-          handicap:     scorecard.handicap,
-          position:     scorecard.team_card.position
-        }
-      end
-    end
-
     attributes :holes do |object|
       holes = object.new_course.holes.sort_by(&:number)
       scores = object.team_scores.sort_by(&:number)
@@ -76,13 +104,18 @@ module RoundInfo
         hole = arr.select { |x| x if x.is_a?(Hole) }.map {|xx| xx}[0]
         score = arr.select { |x| x if x.is_a?(TeamScore) }.map {|xx| xx}[0]
 
+        putt1 = score.blank? ? 0 : score.score_1&.putts
+        putt2 = score.blank? ? 0 : score.score_2&.putts
+        score1 = score.blank? ? 0 : score.score1
+        score2 = score.blank? ? 0 : score.score2
+
         {
           number: num,
           par: hole.par,
           handicap: hole.handicap,
-          score: nil,
+          score: (score1.to_i + score2.to_i),
           net: score.blank? ? 0 : score.net,
-          putts: nil,
+          putts: (putt1.to_i + putt2.to_i),
           hole_id: hole.id,
           score_1_id: score&.score_1_id,
           score_2_id: score&.score_2_id
