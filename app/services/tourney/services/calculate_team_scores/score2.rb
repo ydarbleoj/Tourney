718d94cec3d19f4f08e9eb6c55d1a0924a747430
ydarbleoj <brady.joel@gmail.com>
2021-02-14 13:22:57 -0800

    Update

--- app/services/tourney/services/calculate_team_scores/score2.rb
+++ app/services/tourney/services/calculate_team_scores/score2.rb
@@ -28,30 +28,43 @@ module Tourney
         end
 
         def find_lowest_score
+          p 'hello 99'
           if score2_new?
+            p 'new  score'
             if demote_score1? || both_scores?
+              p 'deom or both'
               [@team_score.score1_id, @team_score.score1]
             else
+              p 'new score inside'
               [new_id, new_net]
             end
           elsif score2_update?
+            p 'update 2'
             if less_than_score1?
+              p 'less than2 '
               [@team_score.score1_id, @team_score.score1]
             elsif less_than_score3?
+              p 'less than 3'
               [@team.score2_id, new_net]
             else
+              p 'update wiht next '
               [next_score_id, next_score_net]
             end
           elsif score1_update?
+            p 'score1 update'
             if both_scores?
+              p 'both'
               if less_than_score2?
+                p 'leshtan2'
                 [@team_score.score1_id, @team_score.score1]
               elsif less_than_score3?
+                p 'less than 3'
                 [@team_score.score1_id, @team_score.score1]
               else
                 [@team_score.score2_id, @team_score.score2]
               end
             elsif less_than_score2?
+              p 'less than 2'
               [@team_score.score2_id, @team_score.score2]
             elsif less_than_score3?
               [@team_score.score1_id, @team_score.score1]
@@ -59,9 +72,12 @@ module Tourney
               [next_score_id, next_score_net]
             end
           elsif new_user_score?
+            p 'new user score '
             if less_than_score2? && !less_than_score1?
+              p 'less than 2 and 1'
               [new_id, new_net]
             else
+              p 'too high '
               [nil, nil]
             end
           end
